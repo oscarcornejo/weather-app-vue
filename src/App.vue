b08c2a380840e770f2e30b324083f93617c6e641
Oscar Cornejo <oscarcornejo10@gmail.com>
2021-04-08 01:41:49 -0400

    App OK 🚀

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="Object.keys(weather).length > 0 && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <InputSearch @getValue="getValue" />
+      <DataWeather :weather="weather" />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputSearch from './components/InputSearch';
+import DataWeather from './components/DataWeather';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    InputSearch,
+    DataWeather,
+  },
+  data() {
+    return {
+      api_key: process.env.VUE_APP_API_KEY,
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      termino: '',
+      weather: {},
+    };
+  },
+  methods: {
+    getValue({ event, value }) {
+      this.termino = value;
+      this.fetchWeather(event, value);
+    },
+    async fetchWeather(e, value) {
+      const urlApi = `${this.url_base}weather?q=${this.termino}&units=metric&APPID=${this.api_key}`;
+      if (e.key == 'Enter') {
+        await fetch(urlApi)
+          .then((res) => res.json())
+          .then((data) => this.setResults(data));
+      }
+    },
+    async setResults(results) {
+      this.weather = results;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+.app {
+  background-image: url('./assets/cold-bg.jpeg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+
+  &.warm {
+    background-image: url('./assets/warm-bg.jpeg');
+  }
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
 </style>
